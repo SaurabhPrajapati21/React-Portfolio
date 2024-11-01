8bdee7ff3d97a63f732421373c7ee8a04bc90c78
SaurabhPrajapati21 <saurabhprajapati7378@gmail.com>
2024-11-01 12:03:26 +0530

    new projects added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.webp";
 import project3 from "../assets/projects/project-3.jpeg";
+import project4 from "../assets/projects/project-4.jpg";
 
 
 export const HERO_CONTENT = `Passionate frontend developer with strong skills in React, Bootstrap, and Tailwind. Experienced in designing and implementing responsive, user-friendly web applications. Dedicated to delivering high-quality, visually appealing digital experiences. Adept at collaborating with teams to bring creative ideas to life. Eager to contribute technical expertise and design acumen to a forward-thinking development team.`;
@@ -40,6 +41,13 @@ export const EDUCATION = [
 
 export const PROJECTS = [
   {
+    title: "NovaStore E-commerce",
+    image: project4,
+    description:
+      "NovaStore is a fully responsive e-commerce site built with React.js and Tailwind CSS, featuring smooth navigation, dynamic sliders, and interactive icons. It includes sections for men’s wear, kids’ wear, and top-rated products for an engaging shopping experience across all device.",
+    url: "https://e-commerce-novasstore.vercel.app/",
+  },
+  {
     title: "Netflix-Clone",
     image: project1,
     description:
@@ -47,18 +55,18 @@ export const PROJECTS = [
     url: "https://web-clone-cyan.vercel.app/",
   },
   {
-    title: "Salesforce Website-Clone",
-    image: project2,
-    description:
-      "A Salesforce website clone created using HTML and CSS, and fully responsive, ensures that the layout and design adapt seamlessly to various screen sizes and devices, providing an optimal user experience across different platforms.",
-    url: "https://saurabhprajapati21.github.io/Salesforce-clone/",
-  },
-  {
     title: "Image-Gallery",
     image: project3,
     description:
       "A responsive image gallery developed with React.js and TailwindCSS, utilizing an API to fetch images. The gallery features a search bar at the top, allowing users to find specific images by name. Designed for optimal viewing across all devices.",
     url: "https://react-image-gallery-eosin.vercel.app/",
+  },
+  {
+    title: "Salesforce Website-Clone",
+    image: project2,
+    description:
+      "A Salesforce website clone created using HTML and CSS, and fully responsive, ensures that the layout and design adapt seamlessly to various screen sizes and devices, providing an optimal user experience across different platforms.",
+    url: "https://saurabhprajapati21.github.io/Salesforce-clone/",
   },
 ];
 
